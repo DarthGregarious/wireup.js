8840657156b3bd476569d770ec755c160d7b73ed
Andrew Kvochick <akvochick@americanappworks.com>
2014-05-24 13:17:26 -0400

    initial commit

---
+++ README.md~
@@ -0,0 +1,4 @@
+wireup.js
+=========
+
+A framework to isolate the DOM and write pure Javascript.
---
+++ wireup-framework.js
@@ -0,0 +1,21 @@
+﻿function WoofTextField() {
+  var self = this;
+  self.onKeyUp = function () { };
+  self.value = function () { };
+}
+
+function WireUp(controller) {
+  var self = this;
+  self.addTextField = function (element, controllerElement) {
+
+    if (controllerElement === undefined || controllerElement.constructor.name !== 'WoofTextField')
+      throw 'Configuration error: the supplied controller property must be a WoofTextField instance.';
+
+    element.onkeyup = controllerElement.onKeyUp;
+    controllerElement.value = function (value) {
+      if (value === undefined)
+        return element.value;
+      element.value = value;
+    }
+  }
+}
